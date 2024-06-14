734f653269026415301a188273169011e34f0fe7
guguidfr <guguidfr@protonmail.com>
2024-06-14 15:24:37 +0200

    Created script for the AWX's first deployment

--- ansible_resources/awx_1st_boot_up.sh
+++ ansible_resources/awx_1st_boot_up.sh
@@ -5,4 +5,3 @@ make docker-compose-build
 make docker-compose COMPOSE_UP_OPTS=-d
 docker exec tools_awx_1 make clean-ui ui-devel
 docker exec -ti tools_awx_1 awx-manage createsuperuser
-echo "@reboot vagrant cd /home/vagrant/awx && make docker-compose COMPOSE_UP_OPTS=-d" >> sudo /etc/crontab
